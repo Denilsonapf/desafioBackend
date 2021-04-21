50cee0f85de1464b24f3c72d53b350acdb700d6b
Denilson <denilson.ferreira@uerj.br>
2021-04-21 16:26:30 -0300

    first commit

---
+++ src/controllers/DeliveryController.js
@@ -0,0 +1,27 @@
+const Delivery = require('../models/Delivery');
+
+module.exports = {
+  
+  //Listar todas as entregas
+    
+    async index( request, response) {
+        const entregas = await Delivery.find();
+
+        return response.json(entregas);
+    },
+  
+  
+  
+    async store(request, response) {
+      
+        const { nome, dataEntrega, pontoPartida, pontoDestino } = request.body;
+        delivery = await Delivery.create({
+            nome,
+            dataEntrega,
+            pontoPartida,
+            pontoDestino,
+        });
+
+        return response.json(delivery);
+  }
+}
